c2f1347ef28bfbbb2c737cf3c93053c3258766d9
dubemoyibe-star <dubemoyibe@gmail.com>
2026-01-31 00:56:42 +0100

    started implementing the books page

--- frontend/src/pages/BooksPage.jsx
+++ frontend/src/pages/BooksPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Books from '../components/Books'
 
 const BooksPage = () => {
   return (
-    <div>
-      Books page
-    </div>
+    <>
+    <Navbar />
+    <Books />
+    <Footer />
+    </>
   )
 }
 
